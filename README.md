e357b96bca36ca3970f24ac32899c6474c4b380b
Tormod Hellen <tormod.hellen@gmail.com>
2018-02-20 22:38:31 +0100

    Working gateway with create and request methods

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # full-stack-learning-project
+
+Requires something a la this in postgres:
+create database mydb;
+create extension "uuid-ossp";
+create table test (id uuid default uuid_generate_v4(), content text);
---
+++ src/main.rs
@@ -0,0 +1,135 @@
+#![feature(plugin)]
+
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+extern crate postgres;
+extern crate rocket_contrib;
+extern crate sql_lexer;
+extern crate uuid;
+extern crate toml;
+
+//#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
+//#[cfg(test)] mod tests;
+
+//use rocket_contrib::{Json, Value};
+use rocket_contrib::{Json, UUID};
+use rocket::State;
+use rocket::response::status;
+use rocket::response::status::{Created, BadRequest, Accepted};
+
+use postgres::{Connection, TlsMode};
+use uuid::Uuid;
+use std::env;
+use std::process;
+use toml::Value;
+use std::fs::File;
+use std::io::prelude::*;
+
+
+#[derive(Serialize)]
+struct SchemalessTable {
+    rows: Vec<SchemalessRow>,
+}
+#[derive(Serialize)]
+struct SchemalessRow {
+    id: Uuid,
+    content: String,
+}
+
+#[derive(Deserialize)]
+struct itemBody {
+    content: String
+}
+
+
+struct Settings {
+    db_url: String
+}
+
+
+fn get_connection(settings: &Settings) -> postgres::Connection 
+{
+    return Connection::connect(settings.db_url.to_string(), TlsMode::None).expect("Could not connect to postgres");
+}
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/hello/<name>/<age>")]
+fn hello_name(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[get("/list")]
+fn list(settings: State<Settings>) -> Json<SchemalessTable>
+{
+    let conn = get_connection(settings.inner());
+    let mut table = SchemalessTable {rows: Vec::new()};
+    for row in &conn.query("SELECT * FROM test", &[]).unwrap() 
+    {
+        table.rows.push(SchemalessRow{id: row.get(0), content: row.get(1)})
+        //println!("Found row {}, with {}", table.id table.content);
+    };
+    Json(table)
+}
+
+#[get("/item/<item>")]
+fn item(item: UUID, settings: State<Settings>) -> Option<Json<SchemalessRow>>
+{
+    let conn = get_connection(settings.inner());
+    let sanitized = sql_lexer::sanitize_string(item.into_inner().hyphenated().to_string());
+    let query = format!("SELECT * FROM test where id = '{}'", sanitized);
+    println!("Query: {}", query);
+    let queryresult = &conn.query(&query, &[]).unwrap();
+    if queryresult.is_empty()
+    {
+        None
+    }
+    else
+    {
+        let row = queryresult.get(0);
+        let result = SchemalessRow{id: row.get(0), content: row.get(1)};
+        Some(Json(result))
+    }
+    
+}
+
+#[post("/item", data = "<item_body>")]
+fn item_post(item_body: Json<itemBody>, settings: State<Settings>) -> Result<Accepted<()>, BadRequest<()>>
+{
+    let conn = get_connection(settings.inner());
+    let sanitized = sql_lexer::sanitize_string(item_body.content.clone());
+    let query = format!("INSERT INTO test (content) VALUES (\'{}\')", sanitized);
+    let queryresult = conn.execute(&query, &[]);
+
+    match queryresult {
+        Ok(o) => return Result::Ok(status::Accepted::<()>(None)),
+        Err(e) => {println!("{}",e); return Result::Err(status::BadRequest::<()>(None))}
+        
+    };
+    
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config_file = match args.iter().find(|&x| x.starts_with("--config-file=")) 
+    {
+        Some(value) => value.trim_left_matches("--config-file="),
+        None => {println!("No config file specified. Exiting."); process::exit(1)}
+    };
+    println!("Config file specified: {}", config_file);
+    let mut file = File::open(config_file).expect("Could not open config file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Could not read config file");
+
+    let settings_list = contents.parse::<Value>().expect("Error while parsing config file");
+    let db_url = settings_list["db_url"].as_str().expect("No db_url specified in config file");
+    println!("db_url: {}", db_url);
+    let settings = Settings{db_url: db_url.to_string()};
+    rocket::ignite().manage(settings).mount("/", routes![hello, hello_name, list, item, item_post]).launch();
+}
